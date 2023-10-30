8c09cc6a798e58b28036066402a2ef841fe34700
Atharva <jaimeen.makavana@gmail.com>
2023-10-30 22:43:35 +0530

    mk

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
+const PORT = process.env.PORT || 3030;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -13,6 +13,6 @@ app.get('/',(req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log('listening to port 3000');
+app.listen(PORT, () => {
+    console.log(`listening to port ${PORT}`);
 })
